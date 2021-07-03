ba49d258d713daa26c5b15656ed80cf6fdf773c6
VladSaichenko <reallinuxforlinux@gmail.com>
2021-07-03 13:02:35 +0700

    Create address model

--- server/apps/services/models/addresses.py
+++ server/apps/services/models/addresses.py
@@ -24,6 +24,17 @@ class Address(models.Model):
         verbose_name='Край/Область',
         max_length=33
     )
+    longitude = models.DecimalField(
+        max_digits=18,
+        decimal_places=3
+    )
+    latitude = models.DecimalField(
+        max_digits=18,
+        decimal_places=3
+    )
+    is_default = models.BooleanField(
+        verbose_name='Основной'
+    )
 
     def __str__(self):
         return f'{self.name}'
@@ -31,3 +42,4 @@ class Address(models.Model):
     class Meta:
         verbose_name = 'адрес'
         verbose_name_plural = 'адреса'
+        unique_together = (('is_default', 'user'),)
